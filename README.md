e6fb63534dd538b8ec762b91a08904e280c82525
wdbacker <wdb@stabe.be>
2017-09-28 12:31:14 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# qewd-cos: helper module for [QEWD](https://www.npmjs.com/package/qewd) to call extrinsic functions in [InterSystems Caché](https://www.intersystems.com/products/cache/)
+
+Thanks to [Rob Tweed](https://github.com/robtweed) for providing the [QEWD](https://www.npmjs.com/package/qewd) server this helper module is meant for.
+
+## Installing
+
+    npm install qewd-cos
+
+## Use
+
+In your QEWD handler module, you can easily use this module to call an extrinsic function you write in Caché:
+
+```javascript
+
+```
+
+## License
+
+ Copyright (c) 2017 Stabe nv,  
+ Hofstade, Oost-Vlaanderen, BE  
+ All rights reserved
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
---
+++ index.js
@@ -0,0 +1,82 @@
+/*
+
+ ----------------------------------------------------------------------------
+ | qewd-cos: helper module for QEWDjs                                       |
+ |                                                                          |
+ | Copyright (c) 2017 Stabe nv,                                             |
+ | Hofstade, Oost-Vlaanderen,                                               |
+ | All rights reserved.                                                     |
+ |                                                                          |
+ | Licensed under the Apache License, Version 2.0 (the "License");          |
+ | you may not use this file except in compliance with the License.         |
+ | You may obtain a copy of the License at                                  |
+ |                                                                          |
+ |     http://www.apache.org/licenses/LICENSE-2.0                           |
+ |                                                                          |
+ | Unless required by applicable law or agreed to in writing, software      |
+ | distributed under the License is distributed on an "AS IS" BASIS,        |
+ | WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. |
+ | See the License for the specific language governing permissions and      |
+ |  limitations under the License.                                          |
+ ----------------------------------------------------------------------------
+
+  28 September 2017
+
+*/
+
+var _tempDataGlobal = 'CacheTempEWDData';
+
+module.exports = {
+  tempDataGlobal: function() {
+    return _tempDataGlobal;
+  },
+
+  jsonResponse: function(error, json) {
+    error = error || '';
+    return {
+      ok: (error === ''),
+      error: error,
+      json: json || {}
+    };
+  },
+
+  restResponse: function(error, json) {
+    if (error && error.statusCode) {
+      return {
+        status: {
+          code: error.statusCode
+        },
+        error: error.error
+      }
+    }
+    else if (error) {
+      return {
+        error: error
+      }
+    }
+    else {
+      return json || {};
+    }
+  },
+
+  doIscFunction: function(iscFunction, params, me, session) {
+    var temp = new me.documentStore.DocumentNode(_tempDataGlobal, [process.pid]);
+    var sessid = (session ? session.id : '');
+
+    temp.delete();
+    temp.setDocument({
+      params : params
+    });
+    var error = me.documentStore.db.function(iscFunction, sessid);
+    var document = temp.getDocument(true);
+    temp.delete();
+    var jsonResponse = {
+      ok : ((error === '') ? true : false),
+      json : document.json || document.results || {},
+      error : error,
+      errors: document.errors || null,
+      warnings: document.warnings || null
+    };
+    return jsonResponse;
+  }
+}
